8db85a64f1b7a8402dd6728506be7ecac204a12e
zhoujumbo <shenqi9507@163.com>
2019-11-26 11:46:05 +0800

    init

--- boot-base/com.basic.support.commons.business/src/main/java/com/basic/support/commons/business/json/JsonClothProcessor.java
+++
@@ -1,7 +0,0 @@
-package com.basic.support.commons.business.json;
-
-import org.json.JSONObject;
-
-public interface JsonClothProcessor {
-	JSONObject wearCloth(Object pojo, JSONObject jsonObject);
-}
